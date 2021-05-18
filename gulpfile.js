bbc6c7152485dd81a6da4639475ecb155669e71c
joeylemon <joeyclemon@gmail.com>
2021-05-18 14:15:45 -0400

    update gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,36 @@
 var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  webserver = require('gulp-webserver');
+  webserver = require('gulp-webserver')
 
-gulp.task('js', function() {
+gulp.task('js', (done) => {
   gulp.src('components/**/*.js')
-});
+  done()
+})
 
-gulp.task('html', function() {
+gulp.task('html', (done) => {
   gulp.src('components/**/*.html')
-});
+  done()
+})
 
-gulp.task('css', function() {
+gulp.task('css', (done) => {
   gulp.src('components/**/*.css')
-});
+  done()
+})
 
-gulp.task('watch', function() {
-  gulp.watch('components/**/*.js', ['js']);
-  gulp.watch('components/**/*.css', ['css']);
+gulp.task('watch', (done) => {
+  gulp.watch('components/**/*.js', gulp.series('js'))
+  gulp.watch('components/**/*.css', gulp.series('css'))
   gulp.watch(['components/**/*.html',
-    'index.html'], ['html']);
-});
+    'index.html'], gulp.series('html'))
+  done()
+})
 
-gulp.task('webserver', function() {
+gulp.task('webserver', (done) => {
   gulp.src('.')
     .pipe(webserver({
       livereload: true,
       open: true
-    }));
-});
+    }))
+  done()
+})
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'webserver']);
+gulp.task('default', gulp.series('watch', 'html', 'js', 'css', 'webserver'))
